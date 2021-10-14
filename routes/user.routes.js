f7b62bc830924a321b4870fd34336d83a7496aeb
Natalia Szlapka <n.machalica@gmail.com>
2021-10-14 13:20:44 +0200

    Add subpages user profile & settings, middleware 'isLogged', logging out and change logging view with google avatar

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/logged', (req, res) => {
-  res.render('logged');
+const isLogged = (req, res, next) => {
+  req.user ? next() : res.redirect('/user/no-permission');
+};
+
+router.get('/logged', isLogged, (req, res) => {
+  res.render('logged', { avatar: req.user.photos[0].value, userName: req.user.displayName });
+});
+
+router.get('/profile', isLogged, (req, res) => {
+  res.render('profile', { avatar: req.user.photos[0].value, userName: req.user.displayName });
+});
+
+router.get('/profile/settings', isLogged, (req, res) => {
+  res.render('settings', { avatar: req.user.photos[0].value, userName: req.user.displayName });
 });
 
 router.get('/no-permission', (req, res) => {
